00c9b08b0e55215a0a8a08304e1607731c81b020
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-06-25 15:41:21 +0300

    v0.37 Using chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,13 +3,20 @@ package main
 import (
 	"BBWA/pkg/config"
 	"BBWA/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
