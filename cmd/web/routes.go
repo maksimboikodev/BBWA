9b7ad854179cbe31f3d66f9c20053066861b9790
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-06-25 15:26:20 +0300

    v0.36 Using pat for routing

---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"BBWA/pkg/config"
+	"BBWA/pkg/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	return mux
+}
