ef0868a1cf61ba0aa625ecedaf664c46174ef3c8
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-06-27 16:52:33 +0300

    v0.38 fix

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/justinas/nosurf"
 	"net/http"
 )
 
-func WriteToConsole(next http.Handler) http.Handler {
+/*func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hit the page")
 		next.ServeHTTP(w, r)
 	})
-}
+}*/
+
 func NotSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
