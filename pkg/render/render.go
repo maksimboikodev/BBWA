75547da77f7ea7e5a9d76e967ca563c7cdc2f5a3
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-07-07 21:09:49 +0300

    v0.59 Creating handlers for our forms & adding CSRF Protection

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -5,6 +5,7 @@ import (
 	"BBWA/pkg/models"
 	"bytes"
 	"fmt"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,11 +21,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	//get the template cash from the app config
 	var tc map[string]*template.Template
 
@@ -39,7 +41,9 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 		log.Fatal("Could not get template from template cache")
 	}
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+
+	td = AddDefaultData(td, r)
+
 	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
