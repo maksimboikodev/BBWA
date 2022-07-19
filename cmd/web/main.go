e4fa28c928356282be511e925275f6f0347c19b6
DESKTOP-3CH4FCF\Maxim <maksimboikodev@gmail.com>
2022-07-19 07:19:52 +0300

    v.080 Centralizing our error handling to a helpers package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"BBWA/internal/config"
 	"BBWA/internal/handlers"
+	"BBWA/internal/helpers"
 	"BBWA/internal/models"
 	"BBWA/internal/render"
 	"encoding/gob"
@@ -10,6 +11,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,6 +19,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 //main is the main application func
 func main() {
@@ -43,6 +47,12 @@ func run() error {
 	//Change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -64,6 +74,7 @@ func run() error {
 
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
